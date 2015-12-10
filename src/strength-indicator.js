757874de173b16d5f03f703803f0a839af1d0872
Joe Macbook Pro <neeaagh@gmail.com>
2015-12-10 11:17:32 -0500

    stubbed strengh engine. implemented basic options.

--- src/strength-indicator.js
+++ src/strength-indicator.js
@@ -1,11 +1,45 @@
 /* ensure $ is meant for jquery */
 (function ( $ ) {
-  $.fn.strengthIndicator = function() {
+  $.fn.strengthIndicator = function(userOptions) {
+    var self = this;
+    var defaults = {
+      score: 0
+    };
+    var options = {};
+    var score = 0;
+
+    var init = function () {
+      /* merge default to options */
+      if (typeof userOptions === 'object') {
+        options = $.extend(defaults, userOptions);
+      }
+      else {
+        options = defaults;
+      }
+      console.log(options);
+      /* render the view */
+      initView();
+    };
+
+    var initView = function() {
+      $('<div class="si-strength-score"></div>').insertAfter(self);
+      updateView();
+    };
+
+    var getSecurityScore = function(password) {
+      this.score =  Math.random() * 100 + 1;
+      updateView();
+    };
+
+    var updateView = function() {
+      $('.si-strength-score').html(this.score);
+    };
 
     this.on('input propertychange', function() {
-      console.log("fired!");
+      getSecurityScore(self.val());
     });
 
+    init();
     return this; /* allow jquery chaining */
   };
 }( jQuery )); /* ensure $ is meant for jquery */
