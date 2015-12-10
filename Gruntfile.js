205860f7a4721cf24f5c455a344dc306dee5481a
Joe Macbook Pro <neeaagh@gmail.com>
2015-12-10 09:46:22 -0500

    ran first grunt task to create file stubs

--- Gruntfile.js
+++ Gruntfile.js
@@ -27,8 +27,8 @@ module.exports = function(grunt) {
 
   // Load the plugin that provides the "uglify" task.
   grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-concat');
 
   // Default task(s).
-  grunt.registerTask('default', ['uglify']);
-
+  grunt.registerTask('default', ['uglify', 'concat']);
 };
