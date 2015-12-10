52d79ae87587f1f89433fe9de500a288c1856424
Joe Macbook Pro <neeaagh@gmail.com>
2015-12-10 11:35:11 -0500

    add rules.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,31 +5,35 @@ module.exports = function(grunt) {
     pkg: grunt.file.readJSON('package.json'),
     concat: {
       options: {
-        banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - */',
+        banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - */\n' +
+          '(function (jQuery) {\n',
+        footer: '}(jQuery));',
         process: function (src, filepath) {
           // Remove all block comments
           return src.replace(/\/\*[\s\S]*?\*\//g, '');
         }
       },
       dist: {
-        src: ['src/<%= pkg.name %>.js'],
+        src: ['src/<%= pkg.name %>.js', 'src/rules.js'],
         dest: 'dist/<%= pkg.name %>.js'
       }
     },
     uglify: {
       options: {
-        banner: '/*! <%= pkg.name %>  - v<%= pkg.version %> - */'
+        banner: '/*! <%= pkg.name %>  - v<%= pkg.version %> - */\n' +
+          '(function (jQuery) {',
+        footer: '}(jQuery));'
       },
       my_target: {
         files: {
-          'dist/<%= pkg.name %>.min.js': ['src/<%= pkg.name %>.js']
+          'dist/<%= pkg.name %>.min.js': ['src/<%= pkg.name %>.js', 'src/rules.js']
         }
       }
     }
   });
 
-  grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-concat');
 
   // Default task(s).
   grunt.registerTask('default', ['concat', 'uglify']);
